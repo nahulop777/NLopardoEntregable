d662a16ab0247021c111da7795eddc3e5ec6bb5b
Lopardo <nahuellopardo@gmail.com>
2019-09-15 00:40:56 -0300

    Parte F

---
+++ src/com/company/Inscripcion.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Date;
+
+public class Inscripcion {
+    private Alumno unAlumno;
+    private Curso unCurso;
+    private Date fechaDelDia;
+
+    public Inscripcion(Alumno unAlumno, Curso unCurso, Date fechaDelDia) {
+        this.unAlumno = unAlumno;
+        this.unCurso = unCurso;
+        this.fechaDelDia = new Date();
+    }
+
+    //Getter
+    public Alumno getUnAlumno() {
+        return unAlumno;
+    }
+    public Curso getUnCurso() {
+        return unCurso;
+    }
+    public Date getFechaDelDia() {
+        return fechaDelDia;
+    }
+
+    //Setter
+    public void setUnAlumno(Alumno unAlumno) {
+        this.unAlumno = unAlumno;
+    }
+    public void setUnCurso(Curso unCurso) {
+        this.unCurso = unCurso;
+    }
+}
