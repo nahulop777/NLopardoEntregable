a2f6e7e0a7d516a1b4812ef453d06f482b80f16f
Lopardo <nahuellopardo@gmail.com>
2019-09-15 00:37:37 -0300

    Parte E

--- src/com/company/Curso.java
+++ src/com/company/Curso.java
@@ -1,15 +1,26 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Curso {
 
     //atributos
     private String nombre;
     private Integer codigoDeCurso;
+    private ProfesorTitular unTitular;
+    private ProfesorAdjunto unAdjunto;
+    private Integer cupoMaximo;
+    List<Alumno> listaDeAlumnos;
 
     //Constructor
-    public Curso(String nombre, Integer codigoDeCurso) {
+    public Curso(String nombre, Integer codigoDeCurso, ProfesorTitular unTitular, ProfesorAdjunto unAdjunto, Integer cupoMaximo) {
         this.nombre = nombre;
         this.codigoDeCurso = codigoDeCurso;
+        this.unTitular = unTitular;
+        this.unAdjunto = unAdjunto;
+        this.cupoMaximo = cupoMaximo;
+        this.listaDeAlumnos = new ArrayList<>();
+
     }
 
     //Getter
@@ -19,6 +30,18 @@ public class Curso {
     public Integer getCodigoDeCurso() {
         return codigoDeCurso;
     }
+    public ProfesorTitular getUnTitular() {
+        return unTitular;
+    }
+    public ProfesorAdjunto getUnAdjunto() {
+        return unAdjunto;
+    }
+    public Integer getCupoMaximo() {
+        return cupoMaximo;
+    }
+    public List<Alumno> getListaDeAlumnos() {
+        return listaDeAlumnos;
+    }
 
     //Setter
     public void setNombre(String nombre) {
@@ -26,5 +49,14 @@ public class Curso {
     }
     public void setCodigoDeCurso(Integer codigoDeCurso) {
         this.codigoDeCurso = codigoDeCurso;
+    }
+    public void setUnTitular(ProfesorTitular unTitular) {
+        this.unTitular = unTitular;
+    }
+    public void setUnAdjunto(ProfesorAdjunto unAdjunto) {
+        this.unAdjunto = unAdjunto;
+    }
+    public void setCupoMaximo(Integer cupoMaximo) {
+        this.cupoMaximo = cupoMaximo;
     }
 }
