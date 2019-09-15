654d1c4a5b70d5160f2ec386799282eda48c3255
Lopardo <nahuellopardo@gmail.com>
2019-09-15 00:20:43 -0300

    Parte A

---
+++ src/com/company/Alumno.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private Integer codigoDeAlumno;
+
+    public Alumno(String nombre, String apellido, Integer codigoDeAlumno) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.codigoDeAlumno = codigoDeAlumno;
+    }
+
+    //Getter
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public Integer getCodigoDeAlumno() {
+        return codigoDeAlumno;
+    }
+
+    
+}
